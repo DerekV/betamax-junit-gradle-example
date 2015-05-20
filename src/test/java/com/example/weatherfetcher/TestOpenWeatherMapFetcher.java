c291a0f3e3c2f0632c9656a9192dd5994cec2885
Derek VerLee <dverlee@maddogtechnology.com>
2015-05-20 18:45:28 -0400

    Add a Junit test demonstrating BetaMax

--- src/test/java/com/example/weatherfetcher/TestOpenWeatherMapFetcher.java
+++ src/test/java/com/example/weatherfetcher/TestOpenWeatherMapFetcher.java
@@ -2,21 +2,29 @@ package com.example.weatherfetcher;
 
 import com.example.weatherfetcher.openweathermap.OpenWeatherMap;
 
+import org.junit.Rule;
 import org.junit.Test;
 
 import java.math.BigDecimal;
 
+import co.freeside.betamax.Betamax;
+import co.freeside.betamax.Recorder;
+import co.freeside.betamax.TapeMode;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertTrue;
 
 public class TestOpenWeatherMapFetcher {
 
-    @Test
-    public void londonLiveTest() {
-        BigDecimal expected = new BigDecimal("284.451");
 
-        BigDecimal actual = WeatherFetcher.fromSource(new OpenWeatherMap()).tempInCity("London,uk");
+    @Rule public Recorder recorder = new Recorder();
+
+    @Betamax(tape="open weather maps api tape",mode= TapeMode.READ_ONLY)
+    @Test
+    public void detroitLiveTest() {
+        BigDecimal expected = new BigDecimal("285.847");
+
+        BigDecimal actual = WeatherFetcher.fromSource(new OpenWeatherMap()).tempInCity("Detroit,mi");
 
         assertThat(actual,is(expected));
     }
